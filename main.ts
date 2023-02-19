49209c7ec34dec8c1a27d3ec975474293d8b9da1
H.Saido <saido.nv@gmail.com>
2023-02-19 18:47:28 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,56 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    recv_timeout = 0
+    if (192 < receivedNumber) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        if (128 < receivedNumber) {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                `)
+        } else {
+            if (64 < receivedNumber) {
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . # . .
+                    . . . . .
+                    . . . . .
+                    `)
+            } else {
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    `)
+            }
+        }
+    }
+})
+let recv_timeout = 0
+radio.setGroup(1)
+basic.forever(function () {
+    radio.sendNumber(input.lightLevel())
+    basic.pause(1000)
+    recv_timeout += 1
+    if (30 < recv_timeout) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
