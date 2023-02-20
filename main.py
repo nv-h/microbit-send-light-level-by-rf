c4c12e70104b6b9e34e3dfa3d14809286c8c3cf3
H.Saido <saido.nv@gmail.com>
2023-02-20 16:51:09 +0900

    receive only block

---
+++ main.py
@@ -0,0 +1,56 @@
+def on_received_number(receivedNumber):
+    global recv_timeout
+    recv_timeout = 0
+    if 192 < receivedNumber:
+        basic.show_leds("""
+            # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+                        # # # # #
+        """)
+    else:
+        if 128 < receivedNumber:
+            basic.show_leds("""
+                . . . . .
+                                . # # # .
+                                . # # # .
+                                . # # # .
+                                . . . . .
+            """)
+        else:
+            if 64 < receivedNumber:
+                basic.show_leds("""
+                    . . . . .
+                                        . . . . .
+                                        . . # . .
+                                        . . . . .
+                                        . . . . .
+                """)
+            else:
+                basic.show_leds("""
+                    . . . . .
+                                        . . . . .
+                                        . . . . .
+                                        . . . . .
+                                        . . . . .
+                """)
+radio.on_received_number(on_received_number)
+
+recv_timeout = 0
+radio.set_group(1)
+
+def on_forever():
+    global recv_timeout
+    radio.send_number(input.light_level())
+    basic.pause(1000)
+    recv_timeout += 1
+    if 30 < recv_timeout:
+        basic.show_leds("""
+            . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -40,17 +40,3 @@ radio.onReceivedNumber(function (receivedNumber) {
 })
 let recv_timeout = 0
 radio.setGroup(1)
-basic.forever(function () {
-    radio.sendNumber(input.lightLevel())
-    basic.pause(1000)
-    recv_timeout += 1
-    if (30 < recv_timeout) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-    }
-})
