7f0429b5e1c686bbad8acddd0809a7d0f3dce2eb
H.Saido <saido.nv@gmail.com>
2023-02-20 16:54:59 +0900

    delete unnecessary

--- main.ts
+++ main.ts
@@ -1,16 +1,5 @@
-let recv_timeout = 0
 radio.setGroup(1)
 basic.forever(function () {
     radio.sendNumber(input.lightLevel())
     basic.pause(1000)
-    recv_timeout += 1
-    if (30 < recv_timeout) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-    }
 })
